9c4e42b586d60f9930caf384b85acafaeee805c8
Agatha Jung <theagathajungg@gmail.com>
2024-04-12 15:27:11 -0700

    incomplete notes

---
+++ workbook-1/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop
+{
+    public static void main(String[] args)
+    {
+        Scanner userInput = new Scanner(System.in);
+        // create variables
+        // ask user for sandwich size
+        // ask users for their names
+        // display cost of sandwich
+
+        // create variables
+        double smallSize;
+        double largeSize;
+        int student;
+
+        System.out.println();
+        System.out.print("Please enter your name: ");
+
+        System.out.printf("Welcome %s. What size sandwich would you like: \n", customerName);
+        System.out.printf("1) %s - $ %.2f \n", PRODUCT_CHOCOLATE, PRODUCT_CHOCOLATE_PRICE);
+        System.out.printf("2) %s - $ %.2f \n", PRODUCT_CANDY, PRODUCT_CANDY_PRICE);
+        System.out.println();
+
+        // ask user for sandwich size
+        System.out.print("What size sandwich would you like? ");
+        if (smallSize);
+        {
+
+        }
+
+        System.out.println("Hello world!");
+    }
+}
--- workbook-1/demos/05-UserInput-starter/src/main/java/com/pluralsight/GroceryStore.java
+++ workbook-1/demos/05-UserInput-starter/src/main/java/com/pluralsight/GroceryStore.java
@@ -2,17 +2,15 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
+import static java.lang.Character.getName;
+
 public class GroceryStore
+
 {
+    Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        Scanner userInput = new Scanner(System.in);
-
-        // declare constants
-        final String PRODUCT_CHOCOLATE = "Milka Aplenmilch";
-        final double PRODUCT_CHOCOLATE_PRICE = 2.99;
-        final String PRODUCT_CANDY = "Skittles";
-        final double PRODUCT_CANDY_PRICE = 1.49;
 
         // input variables
         String customerName;
@@ -22,26 +20,37 @@ public class GroceryStore
         // calculated
         double totalPrice;
         double changeReturned;
+        int productId;
 
 
         // get user input
-        System.out.print("Please enter your name: ");
-        customerName = userInput.nextLine(); // waits for the user to enter their name
+        customerName = getName();
 
         System.out.println();
         System.out.printf("Welcome %s. Today's products are: \n", customerName);
-        System.out.printf("A) %s - $ %.2f \n", PRODUCT_CHOCOLATE, PRODUCT_CHOCOLATE_PRICE);
-        System.out.printf("B) %s - $ %.2f \n", PRODUCT_CANDY, PRODUCT_CANDY_PRICE);
+        System.out.printf("1) %s - $ %.2f \n", PRODUCT_CHOCOLATE, PRODUCT_CHOCOLATE_PRICE);
+        System.out.printf("2) %s - $ %.2f \n", PRODUCT_CANDY, PRODUCT_CANDY_PRICE);
         System.out.println();
+
+        System.out.println("Please select a product: ");
+        productId = userInput.nextInt();
 
         System.out.print("How many would you like to buy? ");
         quantity = userInput.nextInt();
 
         // calculations
-        totalPrice = PRODUCT_CHOCOLATE_PRICE * quantity;
+        if(productId == 1)
+        {
+            totalPrice = PRODUCT_CHOCOLATE_PRICE * quantity;
+            System.out.printf("The total is %.2f \n", totalPrice);
+        }
+        else if(productId == 2)
+        {
+            totalPrice = PRODUCT_CANDY_PRICE * quantity;
+            System.out.printf("The total is %.2f \n", totalPrice);
+        }
 
-        System.out.printf("The total is %.2f \n", totalPrice);
-
+        totalPrice = quantity * productId;
         System.out.print("Please enter your cash amount: ");
         cashReceived = userInput.nextInt();
 
@@ -54,4 +63,17 @@ public class GroceryStore
         System.out.printf("Cash:   $ %6.2f \n", cashReceived);
         System.out.printf("Change: $ %6.2f \n", changeReturned);
     }
+    public static void displayProducts()
+    {
+        final String PRODUCT_CHOCOLATE = "Milka Aplenmilch";
+        final double PRODUCT_CHOCOLATE_PRICE = 2.99;
+        final String PRODUCT_CANDY = "Skittles";
+        final double PRODUCT_CANDY_PRICE = 1.49;
+    }
+    public static String getName()
+    {
+        System.out.print("Please enter your name: ");
+        return userInput.
+    }
+
 }
