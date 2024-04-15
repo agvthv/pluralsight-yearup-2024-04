73c3420b95b5b2faa6c9b14d78c6d898e6e1a201
Agatha Jung <theagathajungg@gmail.com>
2024-04-15 13:55:05 -0700

    FullNameApp

---
+++ workbook-2/FullNameApplication/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication
+{
+    Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        fullName();
+    }
+
+    public static void fullName()
+    {
+        Scanner userInput = new Scanner(System.in);
+
+        // first name
+        System.out.print("Enter your first name: ");
+        String firstName = userInput.nextLine().strip().toUpperCase();
+
+        // middle name
+        System.out.print("Enter your middle name: ");
+        String middleName = userInput.nextLine().strip().toUpperCase();
+
+        // last name
+        System.out.print("Enter your last name: ");
+        String lastName = userInput.nextLine().strip().toUpperCase();
+
+        // print name
+
+        System.out.print(firstName + " ");
+
+        if(!middleName.isBlank())
+        {
+            System.out.print(middleName + " ");
+        }
+        else
+        {
+            middleName = "";
+        }
+
+        System.out.print(lastName + " ");
+
+    }
+}
