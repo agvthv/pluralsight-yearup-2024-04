c16bc9284097bfcb9ce89e9158b234f5681a2f23
Agatha Jung <theagathajungg@gmail.com>
2024-05-08 13:33:41 -0700

    updates to hotel

--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -1,16 +1,15 @@
 package com.pluralsight;
 
-public class Employee
-{
+public class Employee {
     private int employeeID;
     private String employeeName;
     private String department;
-    private int hoursWorked;
+    private double hoursWorked;
     private double payRate;
+    private double startTime;
+    private boolean punchedIn = false;
 
-
-    public Employee(int employeeID, String employeeName, String department, int hoursWorked,double payRate)
-    {
+    public Employee(int employeeID, String employeeName, String department, double hoursWorked, double payRate) {
         this.employeeID = employeeID;
         this.employeeName = employeeName;
         this.department = department;
@@ -18,49 +17,53 @@ public class Employee
         this.payRate = payRate;
     }
 
-    public int getHoursWorked()
-    {
+    public double getHoursWorked() {
         return hoursWorked;
     }
 
-    public String getDepartment()
-    {
+    public String getDepartment() {
         return department;
     }
 
-    public String getEmployeeName()
-    {
+    public String getEmployeeName() {
         return employeeName;
     }
 
-    public int getEmployeeID()
-    {
+    public int getEmployeeID() {
         return employeeID;
     }
 
-    public double getPayRate()
-    {
+    public double getPayRate() {
         return payRate;
     }
 
-    public boolean isOvertime()
-    {
+    public boolean isOvertime() {
         return hoursWorked > 40;
     }
 
-    public double getTotalPay()
-    {
+    public double getTotalPay() {
         return getRegularHours() + getOvertimeHours();
     }
 
-    public double getRegularHours()
-    {
-        return getPayRate() * getHoursWorked();
+    public double getRegularHours() {
+        return payRate * hoursWorked;
     }
 
-    public double getOvertimeHours()
-    {
-        return isOvertime() ? hoursWorked * payRate:0;
+    public double getOvertimeHours() {
+        if (isOvertime()) {
+            double overtimeHours = hoursWorked - 40;
+            return overtimeHours * payRate;
+        }
+        return 0;
+    }
+
+    public void punchTimeCard(double time) {
+        if (!punchedIn) {
+            startTime = time;
+            punchedIn = true;
+        } else {
+            hoursWorked += time - startTime;
+            punchedIn = false;
+        }
     }
 }
-
